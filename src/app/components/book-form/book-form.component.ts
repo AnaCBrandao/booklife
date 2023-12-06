1dd188d5343cdaaa1e4da4e09c3d5661da7d99f0
anaBrandao <anacarolini633@gmail.com>
2023-12-06 17:40:35 -0300

    form validations

--- src/app/components/book-form/book-form.component.ts
+++ src/app/components/book-form/book-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import {FormGroup, FormControl, Validators} from '@angular/forms'
 
 @Component({
   selector: 'app-book-form',
@@ -9,9 +10,31 @@ export class BookFormComponent implements OnInit {
 
   @Input() btnText!:string;
 
+  bookForm!: FormGroup;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.bookForm = new FormGroup({
+      id: new FormControl(''),
+      title: new FormControl('', [Validators.required]),
+      description: new FormControl('', [Validators.required]),
+      image: new FormControl(''),  
+    })
+  }
+
+  get title(){
+    return this.bookForm.get('title')!;
+  }
+
+  get description(){
+    return this.bookForm.get('description')!;
+  }
+
+  submit(){
+    if(this.bookForm.invalid){
+      return;
+    }
   }
 
 }
