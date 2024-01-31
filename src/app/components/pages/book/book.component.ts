ed49649e8ce7f1065dbe85448c050eda4356587c
anaBrandao <anacarolini633@gmail.com>
2024-01-31 20:02:11 -0300

    comments

--- src/app/components/pages/book/book.component.ts
+++ src/app/components/pages/book/book.component.ts
@@ -1,9 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
+import { FormGroup, FormControl, Validators, FormGroupDirective } from '@angular/forms';
 
 import { BookService } from 'src/app/services/book.service';
 import { MessagesService } from 'src/app/services/messages.service';
 import { Book } from 'src/app/interfaces/Book';
+import { Comment } from 'src/app/interfaces/Comment';
+import { CommentService } from 'src/app/services/comment.service';
 
 import { environment } from 'src/environments/environment';
 
@@ -20,18 +23,34 @@ export class BookComponent implements OnInit {
 
   faTimes = faTimes;
   faEdit = faEdit;
+
+  commentForm!: FormGroup;
   
   constructor(
     private bookService: BookService, 
     private route: ActivatedRoute,
     private messagesService: MessagesService,
     private router: Router,
+    private commentService: CommentService,
   ) { }
 
   ngOnInit(): void {
     const id = Number(this.route.snapshot.paramMap.get("id"))
 
-    this.bookService.getBook(id).subscribe((item) => this.book = item.data)
+    this.bookService.getBook(id).subscribe((item) => this.book = item.data);
+
+    this.commentForm = new FormGroup({
+      text: new FormControl("", [Validators.required]),
+      username: new FormControl("", [Validators.required]),
+    })
+  }
+
+  get text() {
+    return this.commentForm.get('text')!;
+  }
+
+  get username() {
+    return this.commentForm.get('username')!;
   }
 
   async removeHandler(id: number) {
@@ -40,6 +59,24 @@ export class BookComponent implements OnInit {
     this.messagesService.add("Livro Excluído com sucesso!")
 
     this.router.navigate(['/']);
+  }
+
+  async onSubmit(formDirective: FormGroupDirective){
+    if(this.commentForm.invalid){
+      return;
+    }
+
+    const data: Comment = this.commentForm.value;
+
+    data.bookId = Number(this.book!.id)
+
+    await this.commentService.createComment(data).subscribe((comment) => this.book!.comments!.push(comment.data));
+
+    this.messagesService.add('Comentário adicionado!');
+
+    this.commentForm.reset();
+
+    formDirective.resetForm();
   }
 
 }
